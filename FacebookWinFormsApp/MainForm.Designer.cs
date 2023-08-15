5917ba19fdab20f0f5bcc961c1c55941f15f12bf
Jonathan Brooker <jonathanbr@mta.ac.il>
2023-08-15 12:35:44 +0300

    login

--- FacebookWinFormsApp/MainForm.Designer.cs
+++ FacebookWinFormsApp/MainForm.Designer.cs
@@ -41,116 +41,123 @@ namespace FacebookDApp
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonLogin
             // 
             this.buttonLogin.BackColor = System.Drawing.Color.White;
             this.buttonLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.buttonLogin.FlatAppearance.BorderSize = 0;
             this.buttonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonLogin.Location = new System.Drawing.Point(12, 71);
-            this.buttonLogin.Name = "button1";
-            this.buttonLogin.Size = new System.Drawing.Size(75, 35);
+            this.buttonLogin.Location = new System.Drawing.Point(14, 89);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(84, 44);
             this.buttonLogin.TabIndex = 0;
             this.buttonLogin.Text = "Login";
             this.buttonLogin.UseVisualStyleBackColor = false;
-            this.buttonLogin.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // buttonLogout
             // 
             this.buttonLogout.BackColor = System.Drawing.Color.White;
             this.buttonLogout.FlatAppearance.BorderSize = 0;
             this.buttonLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonLogout.Location = new System.Drawing.Point(103, 71);
-            this.buttonLogout.Name = "button2";
-            this.buttonLogout.Size = new System.Drawing.Size(75, 35);
+            this.buttonLogout.Location = new System.Drawing.Point(116, 89);
+            this.buttonLogout.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(84, 44);
             this.buttonLogout.TabIndex = 1;
             this.buttonLogout.Text = "Logout";
             this.buttonLogout.UseVisualStyleBackColor = false;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
             // 
-            // pictureBox1
+            // pictureBoxProfile
             // 
-            this.pictureBoxProfile.Location = new System.Drawing.Point(710, 9);
-            this.pictureBoxProfile.Name = "pictureBox1";
-            this.pictureBoxProfile.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxProfile.Location = new System.Drawing.Point(799, 11);
+            this.pictureBoxProfile.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new System.Drawing.Size(225, 250);
             this.pictureBoxProfile.TabIndex = 3;
             this.pictureBoxProfile.TabStop = false;
             // 
-            // label1
+            // labelAppName
             // 
             this.labelAppName.AutoSize = true;
             this.labelAppName.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.labelAppName.ForeColor = System.Drawing.Color.White;
-            this.labelAppName.Location = new System.Drawing.Point(4, 9);
-            this.labelAppName.Name = "label1";
-            this.labelAppName.Size = new System.Drawing.Size(477, 48);
+            this.labelAppName.Location = new System.Drawing.Point(4, 11);
+            this.labelAppName.Name = "labelAppName";
+            this.labelAppName.Size = new System.Drawing.Size(570, 58);
             this.labelAppName.TabIndex = 5;
             this.labelAppName.Text = "FaceBook Desktop App";
             // 
-            // label2
+            // labelCreatePost
             // 
             this.labelCreatePost.AutoSize = true;
             this.labelCreatePost.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.labelCreatePost.ForeColor = System.Drawing.Color.White;
-            this.labelCreatePost.Location = new System.Drawing.Point(18, 139);
-            this.labelCreatePost.Name = "label2";
-            this.labelCreatePost.Size = new System.Drawing.Size(297, 39);
+            this.labelCreatePost.Location = new System.Drawing.Point(20, 174);
+            this.labelCreatePost.Name = "labelCreatePost";
+            this.labelCreatePost.Size = new System.Drawing.Size(348, 46);
             this.labelCreatePost.TabIndex = 6;
             this.labelCreatePost.Text = "Create your Post!";
             this.labelCreatePost.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // textBoxPost
             // 
-            this.textBoxPost.Location = new System.Drawing.Point(17, 181);
+            this.textBoxPost.Location = new System.Drawing.Point(19, 226);
+            this.textBoxPost.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBoxPost.Multiline = true;
-            this.textBoxPost.Name = "textBox1";
-            this.textBoxPost.Size = new System.Drawing.Size(298, 127);
+            this.textBoxPost.Name = "textBoxPost";
+            this.textBoxPost.Size = new System.Drawing.Size(335, 158);
             this.textBoxPost.TabIndex = 7;
             this.textBoxPost.Text = "write your thought...";
             // 
-            // button3
+            // buttonPostNow
             // 
             this.buttonPostNow.BackColor = System.Drawing.Color.White;
             this.buttonPostNow.FlatAppearance.BorderSize = 0;
             this.buttonPostNow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonPostNow.Location = new System.Drawing.Point(17, 314);
-            this.buttonPostNow.Name = "button3";
-            this.buttonPostNow.Size = new System.Drawing.Size(143, 48);
+            this.buttonPostNow.Location = new System.Drawing.Point(19, 392);
+            this.buttonPostNow.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonPostNow.Name = "buttonPostNow";
+            this.buttonPostNow.Size = new System.Drawing.Size(161, 60);
             this.buttonPostNow.TabIndex = 8;
             this.buttonPostNow.Text = "Post Now!";
             this.buttonPostNow.UseVisualStyleBackColor = false;
             // 
-            // button4
+            // buttonFuture
             // 
             this.buttonFuture.BackColor = System.Drawing.Color.White;
             this.buttonFuture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.buttonFuture.FlatAppearance.BorderSize = 0;
             this.buttonFuture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonFuture.Location = new System.Drawing.Point(168, 314);
-            this.buttonFuture.Name = "button4";
-            this.buttonFuture.Size = new System.Drawing.Size(147, 48);
+            this.buttonFuture.Location = new System.Drawing.Point(189, 392);
+            this.buttonFuture.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonFuture.Name = "buttonFuture";
+            this.buttonFuture.Size = new System.Drawing.Size(165, 60);
             this.buttonFuture.TabIndex = 9;
             this.buttonFuture.Text = "Schedule for later!";
             this.buttonFuture.UseVisualStyleBackColor = false;
             // 
-            // button5
+            // buttonDownloadPhotos
             // 
             this.buttonDownloadPhotos.BackColor = System.Drawing.Color.White;
             this.buttonDownloadPhotos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.buttonDownloadPhotos.FlatAppearance.BorderSize = 0;
             this.buttonDownloadPhotos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonDownloadPhotos.Location = new System.Drawing.Point(710, 215);
-            this.buttonDownloadPhotos.Name = "button5";
-            this.buttonDownloadPhotos.Size = new System.Drawing.Size(200, 58);
+            this.buttonDownloadPhotos.Location = new System.Drawing.Point(799, 269);
+            this.buttonDownloadPhotos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonDownloadPhotos.Name = "buttonDownloadPhotos";
+            this.buttonDownloadPhotos.Size = new System.Drawing.Size(225, 72);
             this.buttonDownloadPhotos.TabIndex = 12;
             this.buttonDownloadPhotos.Text = "Download your FaceBook Albums!";
             this.buttonDownloadPhotos.UseVisualStyleBackColor = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(86)))), ((int)(((byte)(147)))));
-            this.ClientSize = new System.Drawing.Size(922, 635);
+            this.ClientSize = new System.Drawing.Size(1037, 794);
             this.Controls.Add(this.buttonDownloadPhotos);
             this.Controls.Add(this.buttonFuture);
             this.Controls.Add(this.buttonPostNow);
@@ -160,6 +167,7 @@ namespace FacebookDApp
             this.Controls.Add(this.pictureBoxProfile);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.buttonLogin);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MainForm";
             this.Text = "FaceBook Desktop App";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
@@ -178,8 +186,6 @@ namespace FacebookDApp
         private System.Windows.Forms.TextBox textBoxPost;
         private System.Windows.Forms.Button buttonPostNow;
         private System.Windows.Forms.Button buttonFuture;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button buttonDownloadPhotos;
     }
 }
--- FacebookWinFormsApp/MainForm.cs
+++ FacebookWinFormsApp/MainForm.cs
@@ -5,21 +5,60 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
 
 namespace FacebookDApp
 {
     public partial class MainForm : Form
     {
+
+        User m_LoggedInUser;
+        LoginResult m_LoginResult;
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void loginAndInit()
         {
+            m_LoginResult = FacebookService.Login("1450160541956417", /// (desig patter's "Design Patterns Course App 2.4" app)
+					"email",
+                    "public_profile",
+                    "user_age_range",
+                    "user_birthday",
+                    "user_events",
+                    "user_friends",
+                    "user_gender",
+                    "user_hometown",
+                    "user_likes",
+                    "user_link",
+                    "user_location",
+                    "user_photos",
+                    "user_posts",
+                    "user_videos");
 
+            if (!string.IsNullOrEmpty(m_LoginResult.AccessToken))
+            {
+                m_LoggedInUser = m_LoginResult.LoggedInUser;
+
+                fetchUserInfo();
+            }
+            else
+            {
+                MessageBox.Show(m_LoginResult.ErrorMessage, "Login Failed");
+            }
+        }
+
+        private void fetchUserInfo()
+        {
+            pictureBoxProfile.LoadAsync(m_LoggedInUser.PictureNormalURL);
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            loginAndInit();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -36,5 +75,13 @@ namespace FacebookDApp
         {
 
         }
+
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            FacebookService.LogoutWithUI();
+            m_LoginResult = null;
+        }
+
+        
     }
 }
