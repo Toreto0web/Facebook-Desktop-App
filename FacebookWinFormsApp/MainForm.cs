de45e5f54240ea3dac026c289f4ccf6e3ad64a5c
Jonathan Brooker <jonathanbr@mta.ac.il>
2023-08-15 16:03:50 +0300

    closest event ready

--- FacebookWinFormsApp/MainForm.cs
+++ FacebookWinFormsApp/MainForm.cs
@@ -95,10 +95,7 @@ namespace FacebookDApp
 
         private void FetchClosestsEvent()
         {
-            Event nearestEvent = new Event();
-            //nearestEvent.UpdateTime = DateTime.Now;
-            //m_LoggedInUser.Events.Add(new Event());
-
+            Event closesestEvent = new Event();
 
             if (m_LoggedInUser.Events.Count == 0)
             {
@@ -107,18 +104,18 @@ namespace FacebookDApp
             }
             else
             {
-                nearestEvent = m_LoggedInUser.Events[0];
+                closesestEvent = m_LoggedInUser.Events[0];
 
                 foreach (Event fbEvent in m_LoggedInUser.Events)
                 {
-                    if (fbEvent.StartTime < nearestEvent.StartTime)
+                    if (fbEvent.StartTime < closesestEvent.StartTime)
                     {
-                        nearestEvent = fbEvent;
+                        closesestEvent = fbEvent;
                     }
                 }
 
-                EventNameLabel.Text = nearestEvent.Name;
-                EventDateLabel.Text = nearestEvent.StartTime.ToString();
+                EventNameLabel.Text = closesestEvent.Name;
+                EventDateLabel.Text = closesestEvent.StartTime.ToString();
             }
         }
 
